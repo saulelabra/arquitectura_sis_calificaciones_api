d4abe61758488b709c3310c23eac432bf0232dee
Jorge De la Vega <jorgedelavg04@gmail.com>
2019-11-24 17:33:20 -0600

    Update grupo.php

--- grupo.php
+++ grupo.php
@@ -1,7 +1,7 @@
 
 <?php
 
-    $materia = $_POST['materia_clave']
+    $materia = $_POST['clave'];
     // PHP Data Objects(PDO) Sample Code:
     try {
         $conn = new PDO("sqlsrv:server = tcp:sistema-calificaciones-db.database.windows.net,1433; Database = sistema-calificaciones", "saulelabra", "ConstruyeDB1");
@@ -15,23 +15,17 @@
     $connectionInfo = array("UID" => "saulelabra", "pwd" => "ConstruyeDB1", "Database" => "sistema-calificaciones", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
     $serverName = "tcp:sistema-calificaciones-db.database.windows.net,1433";
     $conn = sqlsrv_connect($serverName, $connectionInfo);
-    
-    //$query = "SELECT estudiante_matricula, materia_clave, caCad FROM sistemaCalificaciones.Calificacion_estudiante WHERE materia_clave = '$materia'";
-    $query = "SELECT * FROM sistemaCalificaciones.Calificacion_estudiante WHERE materia_clave = 'TC1000'";
+    $query = "SELECT * FROM sistemaCalificaciones.Calificacion_estudiante WHERE materia_clave = '$materia'";
+   
     $stmt = sqlsrv_query( $conn, $query );
     
     /*if( $stmt === false) {
         die( print_r( sqlsrv_errors(), true) );
     }*/
-    
-    echo ("Reading data from table" . PHP_EOL);
+
     
     $json = array();
-     
 
-     if ($stmt == FALSE)
-        echo (sqlsrv_errors());
-   
     while( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_ASSOC) ) {
         $json['estudiante_matricula'] = $row['estudiante_matricula'];
         $json['materia_clave'] = $row['materia_clave'];
@@ -41,7 +35,7 @@
     
     $jsonOut = json_encode($data);
     echo $jsonOut;
-    return ;
+    return;
     
     sqlsrv_free_stmt( $stmt);
 ?>
