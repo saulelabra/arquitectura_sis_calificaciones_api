078d8e13980aa8dd13c0ccd679e3870f0a834ea2
emihdz97 <21973251+emihdz97@users.noreply.github.com>
2019-11-24 23:42:56 -0600

    Update cuatro.php

--- cuatro.php
+++ cuatro.php
@@ -16,24 +16,50 @@
     
     //$query = "SELECT estudianteMatricula, materiaClave, cAcad FROM sistemaCalificaciones.CalificacionEstudiante WHERE estudianteMatricula = '$estudianteMatricula'";
     //$query = "SELECT estudianteMatricula, materiaClave, cAcad, estatus FROM sistemaCalificaciones.CalificacionEstudiante WHERE estudianteMatricula = '12345'";
-    
-    
-
-    $query = "INSERT INTO sistemaCalificaciones.CalificacionEstudiante (estudianteMatricula, materiaClave, cAcad, cEq, cCom, estatus)
-    VALUES ('1111', 'TC2222', '1','1','1','reprobado')";
 
     $stmt = sqlsrv_query($conn, $query);
     if($stmt === false) {
         die(print_r(sqlsrv_errors(), true));
     }
     sqlsrv_free_stmt($stmt);
-
-
-
-    $query = "SELECT * FROM sistemaCalificaciones.CalificacionEstudiante";
-    $stmt = sqlsrv_query($conn, $query);
     
-    $json = array();
+    
+
+    $query = "INSERT INTO sistemaCalificaciones.CalificacionEstudiante (estudianteMatricula, materiaClave, cAcad, cEq, cCom, estatus)
+    VALUES ('12345', 'TC1000', '85','85','85','aprobado')";
+
+    if (sqlsrv_query($conn, $query)) {
+		echo "New record created successfully";
+	} else {
+		echo "Error: " . $query . "<br>" . sqlsrv_error($conn);	
+	}
+
+
+	/*for($i = 0, $size = count($array); $i < $size; ++$i) {
+    	$query = "INSERT INTO sistemaCalificaciones.CalificacionEstudiante (estudianteMatricula, materiaClave, cAcad, cEq, cCom, estatus) 
+    	VALUES(
+    	'"{$array[$i]['estudianteMatricula']}"', 
+    	'"{$array[$i]['materiaClave']}"', 
+    	'"{$array[$i]['cAcad']}"',
+    	'"{$array[$i]['cEq']}"',
+    	'"{$array[$i]['cCom']}"',
+    	'"{$array[$i]['estatus']}"')"
+    	if (sqlsrv_query($conn, $query)) {
+    		echo "New record created successfully";
+		} else {
+    		echo "Error: " . $query . "<br>" . sqlsrv_error($conn);	
+		}
+	}*/
+
+
+   
+
+
+
+    //$query = "SELECT * FROM sistemaCalificaciones.CalificacionEstudiante";
+    //$stmt = sqlsrv_query($conn, $query);
+    
+    /*$json = array();
     while( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_ASSOC) ) {
         $json['estudianteMatricula'] =  $row['estudianteMatricula'];
         $json['materiaClave'] = $row['materiaClave'];
@@ -43,8 +69,8 @@
     }
     
     $jsonOut = json_encode($data);
-    echo $jsonOut;
-    return;
+    echo $jsonOut;*/
+    //return;
     
     sqlsrv_free_stmt( $stmt);
 ?>
