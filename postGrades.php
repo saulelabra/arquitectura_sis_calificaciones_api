64b8e3b506208faab56067126639adafad9262fa
saulelabra <quiquelabra@gmail.com>
2019-11-24 22:21:13 -0600

    fix json in php

--- postGrades.php
+++ postGrades.php
@@ -1,9 +1,6 @@
 <?php
-    $user = $_POST['user'];
 
-    $rawInput = file_get_contents('php://input');
-
-    $inputJson = json_decode($rawInput);
+    $user = $_POST['grades'];
 
     // PHP Data Objects(PDO) Sample Code:
     try {
@@ -14,20 +11,29 @@
         print("Error connecting to SQL Server.");
         die(print_r($e));
     }
+
     // SQL Server Extension Sample Code:
     $connectionInfo = array("UID" => "saulelabra", "pwd" => "ConstruyeDB1", "Database" => "sistema-calificaciones", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
     $serverName = "tcp:sistema-calificaciones-db.database.windows.net,1433";
     $conn = sqlsrv_connect($serverName, $connectionInfo);
-    $query = "SELECT clave, nombre FROM sistemaCalificaciones.Materia WHERE profesorEmail='$inputJson->user'";
+
+    for($i = 0; $i < )
+
+    $query = "SELECT clave, nombre FROM sistemaCalificaciones.Materia WHERE profesorEmail='$user'";
+
     $stmt = sqlsrv_query( $conn, $query);
+
     $json = array();
+
     while( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_ASSOC) ) {
         $json['clave'] = $row['clave'];
         $json['nombre'] = $row['nombre'];
         $data[] = $json;
     }
+
     $jsonOut = json_encode($data);
     echo $jsonOut;
+
     return;
     
     sqlsrv_free_stmt( $stmt);
